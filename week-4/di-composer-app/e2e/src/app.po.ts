5b3bb45ba231950215e446163f9190150f7f14eb
Janet Blohn <jblohn@my365.bellevue.edu>
2020-08-02 14:49:19 -0500

    Add Attribution

--- week-4/di-composer-app/e2e/src/app.po.ts
+++ week-4/di-composer-app/e2e/src/app.po.ts
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title:  app.po.ts
+; Author: Professor Krasso
+; Date:   02 August 2020
+; Modified By: Janet Blohn
+; Description: Exercise 4.2 – Inversion of Control and Dependency Injection
+; AppPage Promise setup
+============================================
+*/
+
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
